bb43423f22747109f69b32538b5b31353b8f7967
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-05-17 11:54:20 +0300

    Handling Errors

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -3,37 +3,52 @@ import { Card } from "../UI/Card";
 import { MealsItem } from "./MealsItem/MealsItem";
 import { useEffect, useState } from "react";
 import { LoadingSpinner } from "../Spinner/LoadingSpinner";
+import { errorsImg } from "../../img";
 
 export const AvailadleMeals = () => {
     const [menu, setMenu] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState();
 
     useEffect(() => {
         fetch('https://react-http-640f4-default-rtdb.europe-west1.firebasedatabase.app/menu.json')
-            .then((data) => data.json()
-                .then((data) => {
-                    let loadedData = [];
-                    for (const key in data) {
-                        loadedData.push({
-                            id: key,
-                            name: data[key].name,
-                            description: data[key].description,
-                            price: data[key].price
-                        })
-                    }
-                    setMenu(loadedData);
-                    setTimeout(() => {
-                        setIsLoading(false);
-                    }, 1000);
-                    
+            .then((data) => {
+                if (!data.ok) {
+                    throw new Error(data.status)
                 }
-                )
+                data.json()
+                    .then((data) => {
+                        let loadedData = [];
+                        for (const key in data) {
+                            loadedData.push({
+                                id: key,
+                                name: data[key].name,
+                                description: data[key].description,
+                                price: data[key].price
+                            })
+                        }
+                        setMenu(loadedData);
+                        setTimeout(() => {
+                            setIsLoading(false);
+                        }, 1000);
+
+                    }
+                    )
+            }
             )
-            .catch((err) => console.log(err))
+            .catch((err) => {
+                console.log(err.message);
+                setHasError(err.message)
+            })
     }, [])
 
+    if (hasError) {
+
+        return <img className={styles.err} src={errorsImg}></img>
+    }
+
     if (isLoading) {
-        return <LoadingSpinner/>
+        return <LoadingSpinner />
     }
 
     const mealsList = menu.map(meal => (
--- src/img.js
+++ src/img.js
@@ -1,3 +1,3 @@
 export const logoBurgerUrl = 'https://static.vecteezy.com/system/resources/previews/007/923/442/original/burger-logo-design-template-illustration-free-vector.jpg'
 export const burgers = 'https://www.hertzpageo.com/img/Hertz_together/Hertz_US_Earned/UK/burgr-las-vegasBAN.jpg'
-
+export const errorsImg = 'https://nebulaconsulting.co.uk/wp-content/uploads/2020/11/mistake-3085712_1280.jpg'
